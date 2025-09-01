2a6623f27d4e2ebfbfb99960713b3dcb03344f02
khanhh <namkhanhle3110@gmail.com>
2025-09-02 00:39:41 +0700

    update html css

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 var typed = new Typed(".text",{
-    strings:["Frontend Developer", "TikToker", "Web Developer"],
+    strings:["Web Developer", "TikToker", "Freelancer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
     loop: true
 });
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 100;
+    const sectionHeight = section.clientHeight;
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === '#' + current) {
+      link.classList.add('active');
+    }
+  });
+};
